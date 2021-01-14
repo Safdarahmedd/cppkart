c1f4589c4cd8149fa9f8293fd9d51afd25c58314
Safdarahmedd <safdarahmed.18@gmail.com>
2021-01-14 16:46:27 +0530

    bug fixes and product file update

--- main.cpp
+++ main.cpp
@@ -42,80 +42,50 @@ public:
 	void input()
 	{
 	system("cls");
-	 gotoxy(75,5);
+	char temp;
+	 gotoxy(53,5);
      cout<<"************";
-     gotoxy(75,1);
+     gotoxy(53,1);
 	 cout<<"Categories:";
-	 gotoxy(75,1);
+	 gotoxy(53,1);
      cout<<"************";
-	 			gotoxy(75,1);
+	 			gotoxy(53,1);
 				cout<<"A. Clothing and Fashion";
-				gotoxy(75,1);
+				gotoxy(53,1);
 				cout<<"B. Electronics";
-				gotoxy(75,1);
+				gotoxy(53,1);
 				cout<<"C. Furniture & Home Decor";
-				gotoxy(75,1);
+				gotoxy(53,1);
 				cout<<"D. Books & Stationery";
-				gotoxy(75,1);
+				gotoxy(53,1);
 				cout<<"E. Games, Music & Entertainment.";
-	gotoxy(75,1);
+	gotoxy(53,1);
 	cout<<"Select category: ";
-	cin>>category;
-	gotoxy(75,3);
+	cin>>temp;
+	category = toupper(temp);
+	gotoxy(53,3);
 	cout<<"Enter name of the product: ";
 	char abc[2];
 	gets(abc);
 	gets(name);
-	gotoxy(75,1);
+	gotoxy(53,1);
 	cout<<"Enter price: ";
 	cin>>price;
 	productCount++;
 	id=productCount;
-	gotoxy(75,3);
+	gotoxy(53,3);
 	cout<<"Product Added. ";
-	gotoxy(75,3);
+	gotoxy(53,3);
 	cout<<"Press any Key to continue....";
 	getch();
   }
 
   void display()
   {
-  gotoxy(75,1);
-  cout<<"*******";
-  gotoxy(75,1);
-  cout<<"PRODUCT : ";
-  gotoxy(75,1);
-  cout<<"*******";
-
-  switch(category)
-  {
-		case 'A':    gotoxy(75,1);
-
-		             cout<<"Category : Clothing and Fashion\n";
-						break;
-
-		case 'B': gotoxy(75,1);
-		          cout<<"Category : Electronics\n";
-						break;
-
-		case 'C': gotoxy(75,1);
-		          cout<<"Category : Furniture & Home Decor\n";
-						break;
-
-		case 'D': gotoxy(75,1);
-		           cout<<"Category : Books & Stationery\n";
-						break;
-
-		case 'E': gotoxy(75,1);
-		           cout<<"Category : Games, Music & Entertainment\n";
-						break;
-		default:    gotoxy(75,3);
-		            cout<<"Wrong Choice";
-	}
-	gotoxy(75,1);
+	gotoxy(53,1);
 	cout<<id<<") ";
     puts(name);
-    gotoxy(75,1);
+    gotoxy(53,1);
     cout<<"Rs."<<price;
 
   }
@@ -139,22 +109,22 @@ public:
 	void input()
 	{
 		system("cls");
-		gotoxy(75,5);
+		gotoxy(53,5);
 		cout<<"*******";
-		gotoxy(75,1);
+		gotoxy(53,1);
 		cout<<"New User";
-		gotoxy(75,1);
+		gotoxy(53,1);
 		cout<<"*******";
-		gotoxy(75,3);
+		gotoxy(53,3);
 		cout<<"Enter name: ";
 		char abc[2];
 		int i;
 		gets(abc);
 		gets(name);
-		gotoxy(75,1);
+		gotoxy(53,1);
 		cout<<"Enter Shipping Address: ";
 		gets(address);
-		gotoxy(75,1);
+		gotoxy(53,1);
 		cout<<"Enter a passcode: ";
         for(i=0; i<4; i++)
             {pass[i]=getch();
@@ -162,9 +132,9 @@ public:
         pass[i]='\0';
      	userCount++;
 		id=userCount;
-		gotoxy(75,1);
+		gotoxy(53,1);
 		cout<<"Profile added successfully.";
-		gotoxy(75,3);
+		gotoxy(53,3);
 	    cout<<"Press Any key to continue...";
 		getch();
 
@@ -172,7 +142,7 @@ public:
 
 	void display()
 	{
-		gotoxy(75,1);
+		gotoxy(53,1);
 		cout<<id<<") ";
 		cout<<"Name: ";
 		puts(name);
@@ -180,10 +150,10 @@ public:
 	}
 
 	void checkout(){
-		gotoxy(75,2);
+		gotoxy(53,2);
 		cout<<"Thank you for your purchase : ";
 		puts(name);
-		gotoxy(75,1);
+		gotoxy(53,1);
 		cout<<"Your order will be delivered in 3-5 bussiness days to : ";
 		puts(address);
 	}
@@ -210,11 +180,11 @@ void display_b()
 	buyer s;
 	f1.read( (char*)&s, sizeof(s) );
 
-		gotoxy(75,5);
+		gotoxy(53,5);
 		cout<<"************";
-		gotoxy(75,1);
+		gotoxy(53,1);
 		cout<<"Profile : ";
-		gotoxy(75,1);
+		gotoxy(53,1);
 		cout<<"************";
 	do{
 		s.display();
@@ -245,9 +215,9 @@ int password2(int b)
      int i;
      again:
      system("cls");
-     gotoxy(75,10);
+     gotoxy(53,10);
      cout<<"Authentication required to proceed!!!!!";
-     gotoxy(75,3);
+     gotoxy(53,3);
      cout<<"Enter The Pin : ";
      for(i=0;i<4;i++)
      {
@@ -257,16 +227,16 @@ int password2(int b)
      ch[i]='\0';
      if(strcmp(ch,pass)==0)
      {
-    gotoxy(75,5);
+    gotoxy(53,5);
 	cout<<"Authentication Completed Successfully....";
 	getch();
 	return 1;
      }
      else
      {
-     	gotoxy(75,4);
+     	gotoxy(53,4);
 	cout<<"Wrong Password Entered,Try Again...";
-	gotoxy(75,2);
+	gotoxy(53,2);
 	cout<<"If you want to try again,press 0..";
 	cin>>a;
 	if(a=='0')
@@ -274,7 +244,7 @@ int password2(int b)
 
 	else
 	{
-	  gotoxy(75,2);
+	  gotoxy(53,2);
 	  cout<<"Authentication Failed....";
 	  return -1;
 	}
@@ -309,8 +279,10 @@ void add_p()
 void categoryMenu();
 void mainMenu();
 
+
 void search_p(char cat)
 {   system("cls");
+    int a[10];
 	cout<<endl;
 	int flag=0;
 	ifstream f1("Product.dat", ios::binary);
@@ -318,8 +290,7 @@ void search_p(char cat)
 	f1.read( (char*)&s, sizeof(s) );
 	do{
 		if(s.retcateg()==cat)
-		 { s.display();
-           cout<<endl;
+		 { a[flag]=s.retid();
            flag++; }
 		f1.read( (char*)&s, sizeof(s) );
 	  }while(f1);
@@ -329,20 +300,72 @@ void search_p(char cat)
          cout<<"No products in this category yet!\n\n";
          cout<<"Press Any Key to go back.";
          getch();
-         categoryMenu();
+         return categoryMenu();
         }
+      else
+      {
+        gotoxy(53,1);
+        cout<<"*******";
+        gotoxy(53,1);
+        cout<<"PRODUCT : ";
+        gotoxy(53,1);
+        cout<<"*******";
+
+        switch(cat)
+        {
+            case 'A':  gotoxy(53,1);
+                        cout<<"Category : Clothing and Fashion\n";
+                            break;
+
+            case 'B': gotoxy(53,1);
+                        cout<<"Category : Electronics\n";
+                            break;
+
+            case 'C': gotoxy(53,1);
+                        cout<<"Category : Furniture & Home Decor\n";
+                            break;
+
+            case 'D': gotoxy(53,1);
+                        cout<<"Category : Books & Stationery\n";
+                            break;
+
+            case 'E': gotoxy(53,1);
+                        cout<<"Category : Games, Music & Entertainment\n";
+                            break;
+            default:    gotoxy(53,3);
+                        cout<<"Wrong Choice";
+                            break;
+        }
+         int z;
+         ifstream f1("Product.dat", ios::binary);
+         for (int j=0;j<flag;j++)
+         {
+            z=a[j];
+            f1.seekg(0);
+            f1.read( (char*)&s, sizeof(s) );
+            do{
+                if(s.retid()== z)
+                {
+                    s.display();
+                    cout<<endl;
+                }
+                f1.read( (char*)&s, sizeof(s) );
+                }while(f1);
+            }
+         f1.close();
+      }
 }
 
-float disp(int n)
+float disp(int items)
 {    system("cls");
-	gotoxy(75,5);
+	gotoxy(53,5);
 	cout<<"*******";
-	gotoxy(93,1);
+	gotoxy(49,1);
 	cout<<"Your purchase:";
-	gotoxy(75,1);
+	gotoxy(53,1);
 	cout<<"*******";
-	 gotoxy(75,2);
-	 if (n==0)
+	 gotoxy(53,2);
+	 if (items==0)
      {
          cout<<"Your Cart is empty!\n\n";
          cout<<" Press Any Key to go back.";
@@ -352,16 +375,16 @@ float disp(int n)
 
      else
      {
-         cout<<"Number of products bought = "<<n<<endl;
-         gotoxy(100,3);
+         cout<<"Number of products bought = "<<items<<endl;
+         gotoxy(80,3);
          cout<<"PRODUCT LIST : ";
-         gotoxy(99,1);
-         cout<<"----------------";
+         gotoxy(79,1);
+         cout<<"----------------\n\n";
          float sum=0;
          int a;
          product x;
          ifstream f1("Product.dat", ios::binary);
-         for (int j=0;j<n;j++)
+         for (int j=0;j<items;j++)
          {
             a=cart[j];
             f1.seekg(0);
@@ -369,7 +392,6 @@ float disp(int n)
             do{
                 if(x.retid()== a)
                 {
-                    cout<<"\n\n\n";
                     x.display();
                     sum+=x.retp();
                     cout<<endl;
@@ -386,31 +408,31 @@ float disp(int n)
 void intro()
 {
  system("cls");
- gotoxy(76,10);
+ gotoxy(57,10);
  cout<<"**********************";
- gotoxy(70,1);
+ gotoxy(50,1);
  cout<<"WELCOME TO OUR E-COMMERCE PLATFORM";
- gotoxy(76,1);
+ gotoxy(54,1);
  cout<<"**********************";
  cout<<"\n\n\n";
- gotoxy(88,1);
+ gotoxy(66,1);
  cout<<"==============================";
- gotoxy(94,1);
+ gotoxy(72,1);
  cout<<"PROJECT MADE BY ::";
  cout<<"\n";
- gotoxy(97,1);
+ gotoxy(75,1);
  cout<<"SAFDAR AHMED";
  cout<<"\n";
- gotoxy(91,1);
+ gotoxy(69,1);
  cout<<"PRABHSIMAR SINGH TANEJA ";
- gotoxy(88,1);
+ gotoxy(66,1);
  cout<<"==============================";
  cout<<"\n\n\n";
- gotoxy(93,3);
+ gotoxy(67,3);
  cout<<"DELHI PUBLIC SCHOOL ";
- gotoxy(76,1);
+ gotoxy(51,1);
  cout<<"==========================================================";
- gotoxy(106,3);
+ gotoxy(81,3);
  cout<<" Press Any Key to continue...";
  getch();
 }
@@ -422,9 +444,9 @@ int password()
      int i;
      again:
      system("cls");
-     gotoxy(75,10);
+     gotoxy(53,10);
      cout<<"Authentication required to proceed!!!!";
-     gotoxy(75,3);
+     gotoxy(53,3);
      cout<<"Enter The Password : ";
      for(i=0;i<4;i++)
      {
@@ -434,16 +456,16 @@ int password()
      ch[i]='\0';
      if(strcmp(ch,passphrase)==0)
      {
-    gotoxy(75,5);
+    gotoxy(53,5);
 	cout<<"Authentication Completed Successfully....";
 	getch();
 	return 1;
      }
      else
      {
-     	gotoxy(75,4);
+     	gotoxy(53,4);
 	cout<<"Wrong Password Entered,Try Again...";
-	gotoxy(75,2);
+	gotoxy(53,2);
 	cout<<"If you want to try again,press 0.. ";
 	cin>>a;
 	if(a=='0')
@@ -484,43 +506,44 @@ void userCountInit()
 
 void categoryMenu()
 {
-    char choice;
+    char choice,temp;
     do{
         system("cls");
-		  gotoxy(75,10);
+		  gotoxy(53,10);
        	cout<<"*******";
-       	  gotoxy(75,1);
+       	  gotoxy(51,1);
 		cout<<"Categories:";
-		  gotoxy(75,1);
+		  gotoxy(53,1);
 	    cout<<"*******";
-	      gotoxy(75,2);
+	      gotoxy(53,2);
         cout<<"A. Clothing and Fashion\n";
-		  gotoxy(75,1);
+		  gotoxy(53,1);
         cout<<"B. Electronics\n";
-		  gotoxy(75,1);
+		  gotoxy(53,1);
         cout<<"C. Furniture & Home Decor\n";
-		  gotoxy(75,1);
+		  gotoxy(53,1);
         cout<<"D. Books & Stationery\n";
-		  gotoxy(75,1);
+		  gotoxy(53,1);
         cout<<"E. Games, Music & Entertainment\n\n";
-          gotoxy(75,1);
+          gotoxy(53,1);
         cout<<"F. Back to menu\n\n";
         char ch;
-          gotoxy(75,3);
+          gotoxy(53,3);
         cout<<"Enter Your Choice : ";
-        cin>>ch;
-        if (ch=='f')
+        cin>>temp;
+        ch = toupper(temp);
+        if (ch=='F')
             return mainMenu();
         search_p(ch);
         int x;
-          gotoxy(75,1);
+          gotoxy(53,1);
         cout<<"Select the product you want to buy: ";
         cin>>x;
         cart[items]=x;
         items++;
-		  gotoxy(75,1);
+		  gotoxy(53,1);
 		cout<<"Do you want to buy more (y/n) : ";
-		cin>>choice;
+		choice=getch();
       }while(choice=='y'||choice=='Y');
 }
 
@@ -531,40 +554,40 @@ void mainMenu()
 	float p;
 	do{
         system("cls");
-          gotoxy(75,10);
+          gotoxy(53,10);
         cout<<"**************";
-          gotoxy(95,1);
+          gotoxy(58,1);
         cout<<"MENU";
-          gotoxy(75,1);
+          gotoxy(53,1);
         cout<<"**************";
-          gotoxy(75,2);
+          gotoxy(53,2);
         cout<<"1. BUY ";
-          gotoxy(75,1);
+          gotoxy(53,1);
         cout<<"2. SELL ";
-          gotoxy(75,1);
+          gotoxy(53,1);
         cout<<"3. Exit";
-          gotoxy(75,3);
+          gotoxy(53,3);
         cout<<"Enter your choice : ";
         cin>>c;
         if(c==2)
             add_p();
         else if(c==1)
-            categoryMenu();
+        {categoryMenu();
         system("cls");
         p=disp(items);
-        if (items==-1)
+        if (p == -1)
             {return mainMenu();
             break;}
         else
             {
-            gotoxy(75,1);
+            gotoxy(53,1);
             cout<<"Your total amount is Rs."<<p;
-              gotoxy(75,1);
+              gotoxy(53,1);
             checkdisplay(currId);
-              gotoxy(75,1);
+              gotoxy(53,1);
             cout<<"Press any key to continue....";
             getch();
-            }
+            }}
     }while (c!=3);
 }
 
@@ -581,21 +604,21 @@ int main()
 	int c,b;
 	do{
 	system("cls");
-	gotoxy(75,10);
+	gotoxy(53,10);
 	cout<<"************";
-	gotoxy(84,1);
+	gotoxy(56,1);
 	cout<<"LOGIN";
-	gotoxy(75,1);
+	gotoxy(53,1);
 	cout<<"************";
-	gotoxy(75,2);
+	gotoxy(53,2);
 	cout<<"1) Create New Profile";
-    gotoxy(75,1);
+    gotoxy(53,1);
 	cout<<"2) Select Existing Profile";
-	gotoxy(75,1);
+	gotoxy(53,1);
 	cout<<"3) EXIT";
-	gotoxy(75,2);
+	gotoxy(53,2);
 	cout<<"************";
-	gotoxy(75,5);
+	gotoxy(53,5);
 	cout<<"Enter your choice : ";
 	cin>>c;
 	if(c==1)
@@ -608,7 +631,7 @@ int main()
     }
 	else cout<<"Wrong input!";
 	} while (c!=2);
-	gotoxy(75,2);
+	gotoxy(53,2);
 	cout<<"Choose profile: ";
 	cin>>b;
 	currId=b;
@@ -617,11 +640,11 @@ int main()
         mainMenu();
 }
 	system("cls");
-	gotoxy(75,5);
+	gotoxy(53,5);
 	cout<<"**************";
-	gotoxy(90,1);
+	gotoxy(54,1);
 	cout<<"THANK YOU!!";
-	gotoxy(75,1);
+	gotoxy(53,1);
 	cout<<"**************";
 }
 
